827ed84d744346b48ab8e5923c4b84d35a8b283c
Yasushi Ando <yasushigit@hogehoge.com>
2025-12-04 12:00:11 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Yasushi Ando',
+    maintainer_email='yasushigit@hogehoge.com',
+    description='a package for practice',
+    license='BSD-3-Clause',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'talker = mypkg.talker:main', #talker.pyのmain関数という意味
+            'listener = mypkg.listener:main', #lisner.pyのmain関数という意味
+        ],
+    },
+)
