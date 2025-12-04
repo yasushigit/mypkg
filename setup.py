08df6050d8818a3a0e0084a7103484628878e2a9
Yasushi Ando <yasushigit@hogehoge.com>
2025-12-04 13:00:30 +0900

    push to dev

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os                  #追加。OSの機能のパッケージ
+from glob import glob
 
 package_name = 'mypkg'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
